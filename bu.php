3f02b529be8a8ec5ed4ba63220d5e2a0551a5cc2
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2016-12-09 16:21:48 +0700

    update news style

--- bu.php
+++ bu.php
@@ -1,18 +1,13 @@
 <?php
-$db = mysqli_connect('localhost','root','root','solohaplaza');
+$db = mysqli_connect('localhost','root','root','ntonline');
 //execute the SQL query and return records
-$result = $db->query("SELECT * FROM `metatag` WHERE entity_id>=33 LIMIT 0 , 9999");
+$result = $db->query("SELECT * FROM `tbl_news` WHERE 1 LIMIT 0 , 9999");
 $data=array();
 while ($row=$result->fetch_assoc()){
-    $data=unserialize($row['data']);
-    $d=array(
-        'title'=>array('value'=>$data['title']['value']),
-        'description'=>array('value'=>$data['keywords']['value']),
-        'keywords'=>array('value'=>$data['description']['value']),
-        );
-   $d=serialize($d);
-    // $sql="UPDATE metatag set data='{$d}' WHERE `entity_type`='node' AND entity_id={$row['entity_id']} AND revision_id={$row['revision_id']}";
-    //$db->query($sql);
+    'http://noithatduckhang.com.vn/Uploaded/nduckhang_35.jpg';
+
+    $data[$row['new_id']]=$row;
 }
+file_put_contents('tbl_news.json',json_encode($data));
 
 ?>
