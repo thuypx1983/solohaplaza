c6fac1209f4e7c17386bf4d4b82c0f3d84f25b27
PHAM XUAN THUY <thuypx1983@gmail.com>
2016-12-17 18:29:00 +0700

    Add style to image

---
+++ abc.php
@@ -0,0 +1,5 @@
+<b>We have 3 great gift ideas for you;<br></b>
+<b>#1 - Smokin' 208 Rub Sampler ($20)</b>&nbsp; - 4 great rubs!<br>
+<b>#2 - Smokin' 208 BBQ Sampler ($25) </b>&nbsp; - Our 4 great rubs<br>&nbsp; &nbsp;  &nbsp; &nbsp; plus a bottle of Extra Virgin Olive Oil Sampler.<br>
+<b>#3 - Smokin' 208 Gift Pack ($50)</b>&nbsp; - Get your BBQ'er <br>&nbsp; &nbsp; &nbsp; &nbsp; going - 2 Large Bottles of Noni’s Olive Oil (Garlic <br>&nbsp; &nbsp; &nbsp; &nbsp; Rosemary and Italian Bread Dipper) plus our 4 <br>&nbsp; &nbsp; &nbsp; &nbsp; excellent BBQ Rubs.<br><br>
+All gift boxes come with recipe cards to help your Backyard BBQ’er get started on a great meal!
