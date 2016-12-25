f3387244358cd8a4a3afcd9f927e9e6891a49cc9
PHAM XUAN THUY <thuypx1983@gmail.com>
2016-12-25 22:00:50 +0700

    nnn

--- sites/all/themes/bootstrap/templates/system/page--front.tpl.php
+++ sites/all/themes/bootstrap/templates/system/page--front.tpl.php
@@ -117,8 +117,7 @@
                 <?php if (!empty($page['highlighted'])): ?>
                     <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
                 <?php endif; ?>
-                <?php if (!empty($breadcrumb)): print $breadcrumb; endif; ?>
-                <a id="main-content"></a>
+
                 <?php print render($title_prefix); ?>
                 <?php if (!empty($title)): ?>
                     <h1 class="page-header"><?php print $title; ?></h1>
