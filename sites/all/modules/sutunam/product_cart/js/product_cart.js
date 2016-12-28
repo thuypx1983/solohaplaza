e115acefe22fcb32a631917ae38c83b7045a5c1e
Pham Xuan Thuy <phamxuanthuy@sutunam.com>
2016-12-28 13:52:59 +0700

    new update

--- sites/all/modules/sutunam/product_cart/js/product_cart.js
+++ sites/all/modules/sutunam/product_cart/js/product_cart.js
@@ -41,39 +41,40 @@ var mobilesize=1280;
                 var quantity=$(this).find('.product-quanity input ').val();
                 var price=$(this).find('.product-quanity .product-price').attr('data-price');
                 var quantity=parseInt(quantity)*parseInt(price);
+                alert(quantity);
                 $(this).find('.product-subtotal').html(number_format(quantity,'.',','))
             })
         }
 
-        function number_format (number, decimals, decPoint, thousandsSep) {
-
-            number = (number + '').replace(/[^0-9+\-Ee.]/g, '')
-            var n = !isFinite(+number) ? 0 : +number
-            var prec = !isFinite(+decimals) ? 0 : Math.abs(decimals)
-            var sep = (typeof thousandsSep === 'undefined') ? ',' : thousandsSep
-            var dec = (typeof decPoint === 'undefined') ? '.' : decPoint
-            var s = ''
-
-            var toFixedFix = function (n, prec) {
-                var k = Math.pow(10, prec)
-                return '' + (Math.round(n * k) / k)
-                        .toFixed(prec)
-            }
-
-            // @todo: for IE parseFloat(0.55).toFixed(0) = 0;
-            s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.')
-            if (s[0].length > 3) {
-                s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep)
-            }
-            if ((s[1] || '').length < prec) {
-                s[1] = s[1] || ''
-                s[1] += new Array(prec - s[1].length + 1).join('0')
-            }
-
-            return s.join(dec)
-        }
 
     })
 
 
 })(jQuery)
+function number_format (number, decimals, decPoint, thousandsSep) {
+
+    number = (number + '').replace(/[^0-9+\-Ee.]/g, '')
+    var n = !isFinite(+number) ? 0 : +number
+    var prec = !isFinite(+decimals) ? 0 : Math.abs(decimals)
+    var sep = (typeof thousandsSep === 'undefined') ? ',' : thousandsSep
+    var dec = (typeof decPoint === 'undefined') ? '.' : decPoint
+    var s = ''
+
+    var toFixedFix = function (n, prec) {
+        var k = Math.pow(10, prec)
+        return '' + (Math.round(n * k) / k)
+                .toFixed(prec)
+    }
+
+    // @todo: for IE parseFloat(0.55).toFixed(0) = 0;
+    s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.')
+    if (s[0].length > 3) {
+        s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep)
+    }
+    if ((s[1] || '').length < prec) {
+        s[1] = s[1] || ''
+        s[1] += new Array(prec - s[1].length + 1).join('0')
+    }
+
+    return s.join(dec)
+}
