80c1982ac6990e613261f55dce2ef920d663bd94
PHAM XUAN THUY <thuypx1983@gmail.com>
2016-12-29 08:13:54 +0700

    a

--- sites/all/modules/imagezoom/js/imagezoom.js
+++ sites/all/modules/imagezoom/js/imagezoom.js
@@ -2,15 +2,12 @@
 
   /**
    * Initialize image zoom functionality.
-
+   */
   Drupal.behaviors.imagezoom = {
     attach: function(context, settings) {
       $('.imagezoom-image', context).elevateZoom(settings.imagezoom);
       console.log(settings.imagezoom);
     }
   }
-   */
-  //initiate the plugin and pass the id of the div containing gallery images
-  $(".imagezoom-image").elevateZoom({gallery:'imagezoom-thumbs', cursor: 'pointer', galleryActiveClass: "active", imageCrossfade: true, loadingIcon: "http://www.elevateweb.co.uk/spinner.gif"});
 
 })(jQuery);
