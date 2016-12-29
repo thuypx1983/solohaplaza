51573d19fee27c01607fd60f7dd2fdb46f1e30e5
PHAM XUAN THUY <thuypx1983@gmail.com>
2016-12-29 08:34:49 +0700

    a

--- sites/all/modules/imagezoom/js/imagezoom.js
+++ sites/all/modules/imagezoom/js/imagezoom.js
@@ -2,15 +2,11 @@
 
   /**
    * Initialize image zoom functionality.
-
+   */
   Drupal.behaviors.imagezoom = {
     attach: function(context, settings) {
       $('.imagezoom-image', context).elevateZoom(settings.imagezoom);
-      console.log(settings.imagezoom);
     }
-  }*/
-  $(document).ready(function(){
-    $("#zoom_03").elevateZoom({gallery:'gallery_01', cursor: 'pointer', galleryActiveClass: "active", imageCrossfade: true, loadingIcon: "http://www.elevateweb.co.uk/spinner.gif"});
+  }
 
-  })
 })(jQuery);
--- sites/all/modules/imagezoom/modules/imagezoom_gallery/theme/imagezoom_gallery.tpl.php
+++ sites/all/modules/imagezoom/modules/imagezoom_gallery/theme/imagezoom_gallery.tpl.php
@@ -9,7 +9,7 @@
  * - $thumbs: An array of thumbnail images.
  */
 ?>
-<!--
+
 <?php print $image; ?>
 
 <div id="imagezoom-thumb-wrapper">
@@ -19,33 +19,3 @@
     <?php endforeach; ?>
   </ul>
 </div>
--->
-<div class="zoom-left">
-    <img style="border:1px solid #e8e8e6;" id="zoom_03" src="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image3.png"
-         data-zoom-image="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/large/image3.jpg"
-         width="411"  />
-
-    <div id="gallery_01" style="width=500px;float:left; ">
-
-    <a  href="#" class="elevatezoom-gallery active" data-update="" data-image="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image1.png"
-        data-zoom-image="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/large/image1.jpg">
-        <img src="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image1.png" width="100"  /></a>
-
-    <a  href="#" class="elevatezoom-gallery"
-        data-image="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image2.png"
-        data-zoom-image="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/large/image2.jpg"
-    ><img src="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image2.png" width="100"  /></a>
-
-    <a href="tester" class="elevatezoom-gallery"
-       data-image="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image3.png"
-       data-zoom-image="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/large/image3.jpg">
-        <img src="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image3.png" width="100"  />
-    </a>
-
-    <a href="tester" class="elevatezoom-gallery"
-       data-image="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image4.png"
-       data-zoom-image="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/large/image4.jpg">
-        <img src="http://www.elevateweb.co.uk/wp-content/themes/radial/zoom/images/small/image4.png" width="100"  /></a>
-
-</div>
-
