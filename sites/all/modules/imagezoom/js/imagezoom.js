429f1ddda1ea34b065a3dbde5b5c0a9ca6ba4dfb
PHAM XUAN THUY <thuypx1983@gmail.com>
2016-12-29 08:21:57 +0700

    image zoom

--- sites/all/modules/imagezoom/js/imagezoom.js
+++ sites/all/modules/imagezoom/js/imagezoom.js
@@ -2,12 +2,13 @@
 
   /**
    * Initialize image zoom functionality.
-   */
+
   Drupal.behaviors.imagezoom = {
     attach: function(context, settings) {
       $('.imagezoom-image', context).elevateZoom(settings.imagezoom);
       console.log(settings.imagezoom);
     }
-  }
+  }*/
+  $("#zoom_03").elevateZoom({gallery:'gallery_01', cursor: 'pointer', galleryActiveClass: "active", imageCrossfade: true, loadingIcon: "http://www.elevateweb.co.uk/spinner.gif"});
 
 })(jQuery);
