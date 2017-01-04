bd34a65fd1032c13b006544298025c8c7b7eb37b
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-01-04 23:02:44 +0700

    category hot

--- sites/all/themes/bootstrap/templates/system/html.tpl.php
+++ sites/all/themes/bootstrap/templates/system/html.tpl.php
@@ -59,6 +59,7 @@
   <link rel="profile" href="<?php print $grddl_profile; ?>" />
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta itemprop="inLanguage" http-equiv="content-language" content="vi" />
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
