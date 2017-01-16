bd9713a074a1fa329e017ad2d234feaf916bcb14
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-01-17 05:44:43 +0700

    main menu

--- sites/all/themes/bootstrap/templates/system/page--front.tpl.php
+++ sites/all/themes/bootstrap/templates/system/page--front.tpl.php
@@ -88,7 +88,7 @@
     <div class="header-bottom">
         <div class="<?php print $container_class; ?>">
             <div class="row">
-                <a href="#mobile-menu"><i class="fa fa-bars"></i></a>
+                <a href="#mobile-menu" class="hidden-desktop hidden-tablet"><i class="fa fa-bars"></i></a>
                 <?php print render($page['header_bottom']); ?>
             </div>
         </div>
