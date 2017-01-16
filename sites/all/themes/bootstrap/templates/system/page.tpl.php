db09ec38b76d3beed30b3a32fe48707e8e793f4f
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-01-17 06:56:08 +0700

    main menu

--- sites/all/themes/bootstrap/templates/system/page.tpl.php
+++ sites/all/themes/bootstrap/templates/system/page.tpl.php
@@ -99,11 +99,36 @@
                     </div>
                     <div class="col-md-9 col-lg-9">
                         <div class="row">
+                            <a href="#mobile-menu" class="hidden-lg hidden-md mobile-menu"><i class="fa fa-bars"></i></a>
                             <?php print render($page['header_bottom']); ?></div>
                     </div>
                 </div>
             </div>
         </div>
+        <?php
+        echo '<nav id="mobile-menu">';
+        function render_menu_tree($menu_tree) {
+            print '<ul>';
+            foreach ($menu_tree as $link) {
+                print '<li>';
+                $link_path = '#';
+                $link_title = $link['link']['link_title'];
+                if($link['link']['link_path']) {
+                    $link_path = drupal_get_path_alias($link['link']['link_path']);
+                }
+                print '<a href="/' . $link_path . '">' . $link_title . '</a>';
+                if(count($link['below']) > 0) {
+                    render_menu_tree($link['below']);
+                }
+                print '</li>';
+            }
+            print '</ul>';
+        }
+        $main_menu_tree = menu_tree_all_data('main-menu', null, 3);
+
+        render_menu_tree($main_menu_tree);
+        echo '</div>';
+        ?>
 
     </div>
 </header>
