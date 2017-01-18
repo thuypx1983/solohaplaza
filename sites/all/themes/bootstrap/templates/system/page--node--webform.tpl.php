c3188ac0f121dcc786676344dd79f71d61029c99
PHAM XUAN THUY <thuypx1983@gmail.com>
2017-01-18 08:30:30 +0700

    new style

--- sites/all/themes/bootstrap/templates/system/page--node--webform.tpl.php
+++ sites/all/themes/bootstrap/templates/system/page--node--webform.tpl.php
@@ -73,40 +73,64 @@
  * @ingroup templates
  */
 ?>
-<header>
-    <div class="bg-header">
-        <div class="header-top">
-            <div class="<?php print $container_class; ?>">
-                <div class="row"><?php print render($page['header_top']); ?></div>
-            </div>
-        </div>
-        <div class="header-center">
-            <div class="<?php print $container_class; ?>">
-                <div class="row"><?php print render($page['header_center']); ?></div>
 
-            </div>
+<div class="bg-header">
+    <div class="header-top">
+        <div class="<?php print $container_class; ?>">
+            <div class="row"><?php print render($page['header_top']); ?></div>
         </div>
-        <div class="header-bottom">
-            <div class="<?php print $container_class; ?>">
-                <div class="row">
-                    <div class="col-md-3 col-lg-3">
-                        <?php
-                        $block = block_load('tb_megamenu', 'main-menu');
-                        $output = @drupal_render(_block_get_renderable_array(_block_render_blocks(array($block))));
-                        print $output;
+    </div>
+    <div class="header-center">
+        <div class="<?php print $container_class; ?>">
+            <div class="row"><?php print render($page['header_center']); ?></div>
 
-                        ?>
-                    </div>
-                    <div class="col-md-9 col-lg-9">
-                        <div class="row">
-                            <?php print render($page['header_bottom']); ?></div>
-                    </div>
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="<?php print $container_class; ?>">
+            <div class="row">
+                <div class="col-md-3 col-lg-3">
+                    <?php
+                    $block = block_load('tb_megamenu', 'main-menu');
+                    $output = @drupal_render(_block_get_renderable_array(_block_render_blocks(array($block))));
+                    print $output;
+
+                    ?>
+                </div>
+                <div class="col-md-9 col-lg-9">
+                    <div class="row">
+                        <a href="#mobile-menu" class="hidden-lg hidden-md mobile-menu"><i class="fa fa-bars"></i></a>
+                        <?php print render($page['header_bottom']); ?></div>
                 </div>
             </div>
         </div>
-
     </div>
-</header>
+    <?php
+    echo '<nav id="mobile-menu">';
+    function render_menu_tree($menu_tree) {
+        print '<ul>';
+        foreach ($menu_tree as $link) {
+            print '<li>';
+            $link_path = '#';
+            $link_title = $link['link']['link_title'];
+            if($link['link']['link_path']) {
+                $link_path = drupal_get_path_alias($link['link']['link_path']);
+            }
+            print '<a href="/' . $link_path . '">' . $link_title . '</a>';
+            if(count($link['below']) > 0) {
+                render_menu_tree($link['below']);
+            }
+            print '</li>';
+        }
+        print '</ul>';
+    }
+    $main_menu_tree = menu_tree_all_data('main-menu', null, 3);
+
+    render_menu_tree($main_menu_tree);
+    echo '</div>';
+    ?>
+</div>
+
 <div class="main-container <?php print $container_class; ?>">
 
     <header role="banner" id="page-header">
